8815873a676d7fe02a3af2a95eb877d345d8a763
Developer Ashish <therobomarket@gmail.com>
2022-07-18 16:39:40 +0530

    login created

--- src/stage_01_load_save.py
+++ src/stage_01_load_save.py
@@ -1,12 +1,18 @@
 from src.utils.all_utils import create_directory, read_yaml, copy_file
 import argparse
 import os
-import pandas as pd
 from pprint import pprint
-import shutil
+import logging
 from tqdm import tqdm
 
-        
+logging_str = "[%(asctime)s:  %(levelname)s: %(module)s]:  %(message)s"
+log_dir = "logs"
+create_directory([log_dir])
+logging.basicConfig(filename = os.path.join(log_dir, "running_logs.log"),
+                                            level = logging.INFO,
+                                            format = logging_str, 
+                                            filemode = 'a')   
+                         
         
 def get_data(config_path):
     config = read_yaml(config_path)
@@ -14,7 +20,7 @@ def get_data(config_path):
     local_data_dirs = config["local_data_dirs"]
     
     for source_download_dir, local_data_dir in tqdm(zip(source_download_dirs, local_data_dirs), total = 2, desc = "List of Folders", colour = 'red'):
-        create_directory([local_data_dirs])
+        create_directory(local_data_dirs)
         copy_file(source_download_dir, local_data_dir)
         
 
@@ -22,6 +28,12 @@ if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--config", "-c", default = "config/config.yaml")
     parsed_args = args.parse_args()
-    get_data(config_path = parsed_args.config)
+    try:
+        logging.info("Stage one started")
+        get_data(config_path = parsed_args.config)
+        logging.info("Stage one completed all the data are saved in local")
+    except Exception as e:
+        logging.exception(e)
+        raise e
      
     
--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -2,28 +2,34 @@ from email import contentmanager
 import yaml
 import os
 import json
+from tqdm import tqdm
+import shutil
+import logging
+
+
 
 def read_yaml(path_to_yaml: str) -> dict:
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
+        logging.info("yaml file: {path_to_yaml} loaded successfully")
     return content
 
 
 def create_directory(dirs: list):
     for dir_path in dirs:
        os.makedirs(dir_path, exist_ok=True) 
-       print(f"Directory is created at {dir_path}")
+       logging.info(f"Directory is created at {dir_path}")
        
 
 def save_local_df(data, data_path):
     data.to_csv(data_path, index = False)
-    print(f"Data is saved at {data_path}")
+    logging.info(f"Data is saved at {data_path}")
     
     
 def save_reports(report: dict, report_path: str):
     with open(report_path, 'w') as f:
         json.dump(report, f, indent = 4)
-    print(f"Reports are saved at {report_path}")   
+    logging.info(f"Reports are saved at {report_path}")   
     
     
 def copy_file(source_download_dir, local_data_dir):
