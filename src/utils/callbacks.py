525d614336e1e6aa49ba8179973be6d838951b50
Developer Ashish <therobomarket@gmail.com>
2022-07-20 15:10:06 +0530

    added ckpt callback

--- src/utils/callbacks.py
+++ src/utils/callbacks.py
@@ -4,6 +4,7 @@ import joblib
 import os
 from src.utils.all_utils import get_timestamp
 
+
 def create_and_save_tensorboard_callback(callbacks_dir, tensorflow_log_dir):
     unique_name = get_timestamp("tb_logs")
     tb_running_log_dir = os.path.join(tensorflow_log_dir, unique_name)
@@ -16,5 +17,12 @@ def create_and_save_tensorboard_callback(callbacks_dir, tensorflow_log_dir):
     
      
 def create_and_save_checkpoint_callback(callbacks_dir, checkpoint_dir):
-    pass
+    checkpoint_file = os.path.join(checkpoint_dir, "ckpt_model.h5")
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+        file_path = checkpoint_file_path, 
+        save_best_only = True
+    )
+    ckpt_callback_filepath = os.path.join(callbacks_dir, "checkpoint_cb.cb")
+    joblib.dump(checkpoint_callback, ckpt_callback_filepath)
+    logging.info(f"model checkpoint is being saved at {ckpt_callback_filepath}")
 
